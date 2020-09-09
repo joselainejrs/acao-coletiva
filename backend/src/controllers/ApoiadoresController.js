0b5e74fc444df25746387c07b70d07d9e4791aba
joselainejrs <joselaine.romao@gmail.com>
2020-09-09 16:48:34 -0300

    front

--- backend/src/controllers/ApoiadoresController.js
+++ backend/src/controllers/ApoiadoresController.js
@@ -2,9 +2,10 @@ const connections = require('../database/connections');
 
 module.exports = {
     async index(request, response) {
-        const totalApoiadores = await connections('dados_pessoais').count()
-    
-        return response.json(totalApoiadores);
+        const [totalApoiadores] = await connections('dados_pessoais').count()
+        
+        console.log(totalApoiadores)
+        return response.json(totalApoiadores['count(*)']);
     },
 
 }
